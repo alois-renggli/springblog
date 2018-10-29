91656a71a8b30b7e33d40db366c7ab1be04448d0
alois-renggli <alois.renggli@gmail.com>
2018-10-29 15:45:40 -0500

    updated lesson for views

--- src/main/java/com/codeup/springblog/HomeController.java
+++ src/main/java/com/codeup/springblog/HomeController.java
@@ -1,15 +1,36 @@
 package com.codeup.springblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
-    @ResponseBody
+    @GetMapping("/home")
     public String sayHello() {
-        return "This is the landing page!";
+        return "home";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String showHello(@PathVariable String name, Model model) {
+        model.addAttribute("name", name.toUpperCase());
+        return "home";
+    }
+
+    @GetMapping("/users")
+    public String showUsers(Model viewModel){
+        List<String> users = new ArrayList<>();
+        users.add("louie");
+        users.add("mike");
+        users.add("tim");
+        users.add("john");
+        users.add("paul");
+        viewModel.addAttribute("users", users);
+        return "users";
     }
 }
 
